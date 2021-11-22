5a5a1ba0cd25e062bb91d45e87c977e522c49d4a
VMoskalenko <vladyslav.moskalenko@microfocus.com>
2021-11-22 23:24:35 +0200

    Format all files in project

--- server/MailService.js
+++ server/MailService.js
@@ -1,13 +1,13 @@
-let nodemailer = require("nodemailer");
-require("dotenv").config();
+let nodemailer = require('nodemailer');
+require('dotenv').config();
 
 class MailService {
     static transporter = nodemailer.createTransport({
-        service: "gmail",
+        service: 'gmail',
         auth: {
             user: process.env.FROM,
-            pass: process.env.PASSWORD
-        }
+            pass: process.env.PASSWORD,
+        },
     });
 
     constructor(subject, message) {
@@ -15,32 +15,31 @@ class MailService {
             from: process.env.FROM,
             to: process.env.TO,
             subject,
-            text: message
+            text: message,
         };
     }
 
     async sendMail() {
         return new Promise((resolve, reject) => {
-            MailService.transporter.sendMail(this.mailOptions, function (error, info) {
-                if (error) {
-                    console.log(error);
-                    return resolve({
-                        status: "BAD",
-                        message: error
-                    });
-                } else {
-                    return resolve({
-                        status: "OK",
-                        message: "Email successfully sent!!!"
-                    });
-                }
-            });
-        })
+            MailService.transporter.sendMail(
+                this.mailOptions,
+                function (error, info) {
+                    if (error) {
+                        console.log(error);
+                        return resolve({
+                            status: 'BAD',
+                            message: error,
+                        });
+                    } else {
+                        return resolve({
+                            status: 'OK',
+                            message: 'Email successfully sent!!!',
+                        });
+                    }
+                },
+            );
+        });
     }
 }
 
-module.exports = {MailService};
-
-
-
-
+module.exports = { MailService };
--- src/SendEmailForm.js
+++ src/SendEmailForm.js
@@ -1,31 +1,32 @@
-import React, {useEffect, useState} from 'react';
-import {useHttp} from "./useHttp";
+import React, { useEffect, useState } from 'react';
+import { useHttp } from './useHttp';
 
 let needToAddEventListeners = true;
 let needToCheckValidation = false;
 let form;
 
-const SendEmailForm = ({showLoader, hideLoader}) => {
+const SendEmailForm = ({ showLoader, hideLoader }) => {
     const [isValid, setIsValid] = useState(true);
-    const {loading, request, cleanErrors} = useHttp();
-    const [state, setState] = useState({firstName: "", lastName: "", subject: "", message: ""});
+    const { loading, request, cleanErrors } = useHttp();
+    const [state, setState] = useState({
+        firstName: '',
+        lastName: '',
+        subject: '',
+        message: '',
+    });
     useEffect(() => {
-        if (loading)
-            showLoader();
-        else
-            hideLoader();
+        if (loading) showLoader();
+        else hideLoader();
     }, [loading]);
 
     const changeState = (key, newValue) => {
-        setState(prevState => ({...prevState, [key]: newValue}));
-    }
+        setState(prevState => ({ ...prevState, [key]: newValue }));
+    };
     useEffect(() => {
         if (needToAddEventListeners) {
             needToAddEventListeners = false;
             form = document.querySelector('.needs-validation');
-            form.addEventListener('change', function (event) {
-
-            }, false)
+            form.addEventListener('change', function (event) {}, false);
         }
     }, []);
 
@@ -38,9 +39,9 @@ const SendEmailForm = ({showLoader, hideLoader}) => {
         } else {
             setIsValid(false);
         }
-    })
+    });
 
-    const sendEmail = async (event) => {
+    const sendEmail = async event => {
         event.preventDefault();
         needToCheckValidation = true;
         if (!form.checkValidity()) {
@@ -48,74 +49,109 @@ const SendEmailForm = ({showLoader, hideLoader}) => {
             return;
         }
         try {
-            let response = await request('http://localhost:5000/api/send-mail', "POST", {
-                firstName: state.firstName,
-                lastName: state.lastName,
-                subject: state.subject,
-                message: state.message
-            });
+            let response = await request(
+                'http://localhost:5000/api/send-mail',
+                'POST',
+                {
+                    firstName: state.firstName,
+                    lastName: state.lastName,
+                    subject: state.subject,
+                    message: state.message,
+                },
+            );
             alert(response.message);
         } catch (err) {
             cleanErrors();
         } finally {
             needToCheckValidation = false;
         }
-    }
+    };
     return (
-        <div className='container mt-2'>
+        <div className="container mt-2">
             <form className="row g-3 needs-validation" noValidate>
                 <div className="md">
-                    <label htmlFor="firstName" className="form-label">First name</label>
-                    <input type="text" className="form-control" id="firstName" value={state.firstName}
-                           onChange={(event) => changeState("firstName", event.target.value)} required/>
-                    <div className="valid-feedback">
-                        Validated!
-                    </div>
-                    <div className="invalid-feedback">
-                        Invalid!
-                    </div>
+                    <label htmlFor="firstName" className="form-label">
+                        First name
+                    </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="firstName"
+                        value={state.firstName}
+                        onChange={event =>
+                            changeState('firstName', event.target.value)
+                        }
+                        required
+                    />
+                    <div className="valid-feedback">Validated!</div>
+                    <div className="invalid-feedback">Invalid!</div>
                 </div>
                 <div className="md">
-                    <label htmlFor="lastName" className="form-label">Last name</label>
-                    <input type="text" className="form-control" id="lastName" value={state.lastName}
-                           onChange={(event) => changeState("lastName", event.target.value)} required/>
-                    <div className="valid-feedback">
-                        Validated!
-                    </div>
-                    <div className="invalid-feedback">
-                        Invalid!
-                    </div>
+                    <label htmlFor="lastName" className="form-label">
+                        Last name
+                    </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="lastName"
+                        value={state.lastName}
+                        onChange={event =>
+                            changeState('lastName', event.target.value)
+                        }
+                        required
+                    />
+                    <div className="valid-feedback">Validated!</div>
+                    <div className="invalid-feedback">Invalid!</div>
                 </div>
                 <div className="md">
-                    <label htmlFor="subject" className="form-label">Subject</label>
-                    <input type="text" className="form-control" id="subject" value={state.subject}
-                           onChange={(event) => changeState("subject", event.target.value)} required/>
-                    <div className="valid-feedback">
-                        Validated!
-                    </div>
-                    <div className="invalid-feedback">
-                        Invalid!
-                    </div>
+                    <label htmlFor="subject" className="form-label">
+                        Subject
+                    </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="subject"
+                        value={state.subject}
+                        onChange={event =>
+                            changeState('subject', event.target.value)
+                        }
+                        required
+                    />
+                    <div className="valid-feedback">Validated!</div>
+                    <div className="invalid-feedback">Invalid!</div>
                 </div>
                 <div className="md">
-                    <label htmlFor="message" className="form-label">Email message</label>
-                    <input type="text" className="form-control" id="message" value={state.message}
-                           onChange={(event) => changeState("message", event.target.value)} required/>
-                    <div className="valid-feedback">
-                        Validated!
-                    </div>
-                    <div className="invalid-feedback">
-                        Invalid!
-                    </div>
+                    <label htmlFor="message" className="form-label">
+                        Email message
+                    </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="message"
+                        value={state.message}
+                        onChange={event =>
+                            changeState('message', event.target.value)
+                        }
+                        required
+                    />
+                    <div className="valid-feedback">Validated!</div>
+                    <div className="invalid-feedback">Invalid!</div>
                 </div>
                 <div className="col-12">
-                    <button className="btn btn-primary" type="button" onClick={sendEmail}
-                            disabled={loading || (!isValid && needToCheckValidation)}>Submit form
+                    <button
+                        className="btn btn-primary"
+                        type="button"
+                        onClick={sendEmail}
+                        disabled={
+                            loading || (!isValid && needToCheckValidation)
+                        }
+                    >
+                        Submit form
                     </button>
                 </div>
             </form>
         </div>
     );
-}
+};
 
 export default SendEmailForm;
